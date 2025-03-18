ebab05addf94901eb71688bc4e72319a01b65abc
nakib360 <fliptechofficial@gmail.com>
2025-03-18 22:52:43 +0600

    updating input size

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-function addValue(value){
-    const field = document.getElementById("calculate-feild");
-    document.getElementById("calculate-feild").value += value;
+const field = document.getElementById("calculate-feild");
+
+
+function updateFontSize() {
+    if (field.value.length >= 20) {
+        field.style.fontSize = "20px";
+    } else if (field.value.length >= 14) {
+        field.style.fontSize = "28px";
+    } else {
+        field.style.fontSize = "36px";
+    }
+}
+
+function addValue(value) {
+    field.value += value;
+    updateFontSize(); 
+
     setTimeout(() => {
         field.scrollLeft = field.scrollWidth;
     }, 10);
 }
 
-function deleteValue (){
-    document.getElementById("calculate-feild").value = document.getElementById("calculate-feild").value.slice(0, -1);
+function deleteValue() {
+    field.value = field.value.slice(0, -1);
+    updateFontSize(); 
 }
 
-function formatInput(){
-    document.getElementById("calculate-feild").value = ''
+function formatInput() {
+    field.value = "";
+    document.getElementById("result-field").innerText = "";
+    updateFontSize(); 
 }
 
-
-function result(){
-    try{
-        document.getElementById("calculate-feild").value = eval(document.getElementById("calculate-feild").value);
-    }catch{
-        document.getElementById("calculate-feild").value = "error syntex"
+function result() {
+    try {
+        document.getElementById("result-field").innerText = eval(field.value);
+    } catch {
+        document.getElementById("result-field").innerText = "error syntex";
     }
 }
